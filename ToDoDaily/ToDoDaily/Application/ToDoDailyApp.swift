09aafec1212c95ab686a70c97ad6c6f82dd4a83c
Igor Kulik <phitochai@gmail.com>
2021-12-16 16:58:11 +0200

    Added SwiftGen to the project, created launch screen

---
+++ ToDoDaily/ToDoDaily/Application/ToDoDailyApp.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoDailyApp.swift
+//  ToDoDaily
+//
+//  Created by Igor Kulik on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDailyApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
